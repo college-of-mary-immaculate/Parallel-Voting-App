c7765a46badda4ec8a5e3f3c4e66becb310351bf
marygraceparedes <paredesmarygrace71@gmail.com>
2026-03-02 00:02:37 +0800

    update in utils/index.js

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,2 +1,7 @@
 // Utils index file
 // Export all utility functions from here for easier imports
+
+export { default as api, handleApiError, endpoints } from './axios';
+
+// Add other utility exports here as needed
+// export { formatDate, validateEmail, etc. } from './otherUtils';
